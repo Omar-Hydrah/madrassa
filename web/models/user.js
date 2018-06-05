07dc071b81494e7f443eed88f80f8d2cfd5f66ca
Omar Hydrah <omarhydrah@tutanota.de>
2018-06-05 18:42:05 +0200

    Update user model to match database structure
    `users` table has been changed in the database, to only contain 5 fields:
    user_id, username, password, role ("admin", "teacher", "student"), created_at.
    This is to prevent the duplication of data across three different tables.

--- web/models/user.js
+++ web/models/user.js
@@ -7,25 +7,19 @@ module.exports = function(sequelize, DataTypes) {
 			autoIncrement : true,
 			primaryKey :true
 		}, 
-		first_name : {
-			type: DataTypes.STRING(15),
+		username : {
+			type: DataTypes.STRING(30),
 			allowNull: false,
 
 		},
 
-		last_name : {
-			type :DataTypes.STRING(15),
+		password : {
+			type :DataTypes.STRING(20),
 			allowNull: false
 		}, 
 
-		email : {
-			type: DataTypes.STRING(40),
-			allowNull: false,
-		},
-
 		role : {
 			type: DataTypes.ENUM("admin", "teacher", "student"),
-			allowNull: false
 		}, 
 
 		created_at : {
