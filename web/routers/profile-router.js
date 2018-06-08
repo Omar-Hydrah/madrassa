3491bc902c8ee13d48bfd6e45a69c557e083efb5
Omar Hydrah <omarhydrah@tutanota.de>
2018-06-08 22:07:39 +0200

    Delete unnecessary routers, and remove commented-out code

--- web/routers/profile-router.js
+++ web/routers/profile-router.js
@@ -19,29 +19,4 @@ router.get("/admin", (req, res)=>{
 	});
 });
 
-/*router.post("/set-role", (req, res)=>{
-	var allowedRoles = ["teacher", "student"];
-
-	// Confirm that there's a user role, and that it's an allowed role.
-	if(req.body.userRole && allowedRoles.indexOf(req.body.userRole) != -1){
-
-		UserController.updateRole(req.session.user.userId, req.body.userRole)
-			.then((user)=>{
-				// Raw user is sent from the controller.
-				console.log(user);
-				req.session.user.role = user.role;
-
-				//res.redirect("/profile", {
-				//	message: "success"
-				//});
-				return res.redirect("/profile");
-			}).catch((err)=>{
-				return res.redirect("/profile");
-			});
-		return;	
-	}
-
-	return res.redirect("/profile");
-});
-*/
 module.exports = router;
