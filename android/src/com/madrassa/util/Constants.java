8dcc48094b8fb6db4d9f88b9fd1ea205102f51ec
Omar Hydrah <omarhydrah@tutanota.de>
2018-09-08 20:49:21 +0200

    Create Constants file, and include okhttp3 logging dependency

---
+++ android/src/com/madrassa/util/Constants.java
@@ -0,0 +1,7 @@
+package com.madrassa.util;
+
+
+public class Constants{
+	public static final String BASE_URL = "http://192.168.1.103/";
+	public static final String USER_AGENT = "Madrassa-Application";
+}
