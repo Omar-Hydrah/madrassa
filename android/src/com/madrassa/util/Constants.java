e99c26235b172ffa5810f57cf8a0622513740b68
Omar Hydrah <omarhydrah@tutanota.de>
2018-09-25 23:15:18 +0200

    Rename the `TAG` field to `LOG_TAG` in Constants.java

--- android/src/com/madrassa/util/Constants.java
+++ android/src/com/madrassa/util/Constants.java
@@ -4,5 +4,5 @@ package com.madrassa.util;
 public class Constants{
 	public static final String BASE_URL   = "http://192.168.1.103/api/";
 	public static final String USER_AGENT = "Madrassa-Application";
-	public static final String TAG        = "madrassa";
+	public static final String LOG_TAG    = "madrassa";
 }
