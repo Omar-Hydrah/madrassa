10898310d344a578af93d70ff0daf8e3e24b9bec
Omar Hydrah <omarhydrah@tutanota.de>
2018-09-27 20:13:02 +0200

    Create CourseDetail activity

---
+++ android/src/com/madrassa/CourseDetailActivity.java
@@ -0,0 +1,15 @@
+package com.madrassa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class CourseDetailActivity extends AppCompatActivity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_course_detail);
+	}
+}
