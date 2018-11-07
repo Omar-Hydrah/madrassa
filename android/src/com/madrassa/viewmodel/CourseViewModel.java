b0348876b2d112040da100060b8b87b72977a2ae
Omar Hydrah <omarhydrah@tutanota.de>
2018-11-07 17:00:28 +0200

    Remove commented code from CourseViewModel

--- android/src/com/madrassa/viewmodel/CourseViewModel.java
+++ android/src/com/madrassa/viewmodel/CourseViewModel.java
@@ -22,12 +22,6 @@ import com.madrassa.MadrassaApplication;
 public class CourseViewModel{
 
 	private AppRepository repo;
-	// public MutableLiveData<CourseResponse> courseResponse = 
-	// 	new MutableLiveData<CourseResponse>();
-	// public MutableLiveData<BooleanResponse> joinCourseResponse = 
-	// 	new MutableLiveData<BooleanResponse>();
-	// public MutableLiveData<BooleanResponse> leaveCourseResponse =
-	// 	new MutableLiveData<BooleanResponse>();
 	Single<CourseResponse> courseResponse;
 	Single<BooleanResponse> joinCourseResponse;
 	Single<BooleanResponse> leaveCourseResponse;
@@ -40,36 +34,15 @@ public class CourseViewModel{
 		return repo.getCourse(id)
 			.subscribeOn(Schedulers.io());
 
-		// .observeOn(AndroidSchedulers.mainThread())
-		// .subscribe(courseResponse -> {
-		// 	// Log.i(Constants.TAG, courseResponse.toString());
-		// 	this.courseResponse.postValue(courseResponse);
-		// }, throwable -> {
-		// 	Log.i(Constants.TAG, "Error occurred");
-		// });
 	}
 
 	public Single<BooleanResponse> joinCourse(int courseId){
 		return repo.joinCourse(courseId)
 			.subscribeOn(Schedulers.io());
-
-		// .observeOn(AndroidSchedulers.mainThread())
-		// .subscribe(booleanResponse ->{
-		// 	this.joinCourseResponse.postValue(booleanResponse);
-		// }, throwable ->{
-		// 	Log.i(Constants.TAG, "Error occurred");
-		// });
 	}
 
 	public Single<BooleanResponse> leaveCourse(int courseId){
 		return repo.leaveCourse(courseId)
 			.subscribeOn(Schedulers.io());
-
-		// .observeOn(AndroidSchedulers.mainThread())
-		// .subscribe(booleanResponse ->{
-		// 	this.leaveCourseResponse.postValue(booleanResponse);
-		// }, throwable ->{
-		// 	Log.i(Constants.TAG, "Error occurred");
-		// });
 	}
 }
